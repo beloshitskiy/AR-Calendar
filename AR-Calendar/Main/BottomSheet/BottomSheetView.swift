62e5fd3c999a8bad75165ba0888d9b19a9995400
Denis Beloshitskiy <beloshitskiy.denis@gmail.com>
2022-11-16 23:55:27 +0300

    Add MainVC and BottomSheet

---
+++ AR-Calendar/Main/BottomSheet/BottomSheetView.swift
@@ -0,0 +1,130 @@
+//
+//  BottomSheetView.swift
+//  AR-Calendar
+//
+//  Created by Denis Beloshitskiy
+//
+
+import SnapKit
+import UIKit
+
+final class BottomSheetView: UIStackView {
+  convenience init() {
+    self.init(frame: .zero)
+    [aboutImageView, aboutIETView].forEach { self.addArrangedSubview($0) }
+    setupConstraints()
+  }
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+
+  @available(*, unavailable)
+  required init(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  func updateContent(with month: MonthsModel.Month) {
+    print(aboutImageView.subviews)
+    print(aboutIETView.subviews)
+  }
+
+  private func setupConstraints() {
+    aboutImageView.snp.makeConstraints { $0.horizontalEdges.equalToSuperview() }
+    aboutIETView.snp.makeConstraints { $0.horizontalEdges.equalToSuperview() }
+  }
+
+  private let aboutImageView: UIView = {
+    let view = UIView()
+    view.backgroundColor = Colors.lightGray
+
+    let imageView = UIImageView(image: UIImage(named: "infoIcon"))
+    imageView.contentMode = .scaleAspectFit
+
+    let infoImageTitle = UILabel()
+    infoImageTitle.numberOfLines = 0
+    infoImageTitle.font = .boldSystemFont(ofSize: 18)
+    infoImageTitle.textColor = Colors.darkGray
+
+    let infoImageTextView = UITextView()
+    infoImageTextView.font = .systemFont(ofSize: 16)
+    infoImageTextView.backgroundColor = .clear
+    infoImageTextView.textColor = Colors.darkGray
+    infoImageTextView.contentMode = .left
+    infoImageTextView.showsVerticalScrollIndicator = false
+    infoImageTextView.isEditable = false
+    infoImageTextView.isScrollEnabled = false
+
+    [imageView, infoImageTitle, infoImageTextView].forEach { view.addSubview($0) }
+
+    imageView.snp.makeConstraints { make in
+      make.size.equalTo(50)
+      make.top.equalToSuperview().offset(20)
+      make.leading.equalToSuperview().offset(20)
+    }
+
+    infoImageTitle.snp.makeConstraints { make in
+      make.top.equalToSuperview()
+      make.leading.equalToSuperview().offset(20)
+      make.trailing.equalToSuperview().offset(-20)
+    }
+
+    infoImageTextView.snp.makeConstraints { make in
+      make.top.equalTo(infoImageTitle.snp.bottom)
+      make.leading.equalTo(imageView.snp.trailing).offset(10)
+      make.trailing.equalToSuperview().offset(-20)
+    }
+
+    return view
+  }()
+
+  private let aboutIETView: UIView = {
+    let view = UIView()
+    view.backgroundColor = Colors.darkGray
+
+    let ietImageView = UIImageView(image: UIImage(named: "IETIcon"))
+    ietImageView.contentMode = .scaleAspectFit
+
+    let ietTextView = UITextView()
+    ietTextView.textColor = Colors.lightGray
+    ietTextView.font = .systemFont(ofSize: 16)
+    ietTextView.backgroundColor = .clear
+    ietTextView.contentMode = .left
+    ietTextView.showsVerticalScrollIndicator = false
+    ietTextView.isEditable = false
+
+    [ietImageView, ietTextView].forEach { view.addSubview($0) }
+
+    ietImageView.snp.makeConstraints { make in
+      make.size.equalTo(50)
+      make.leading.top.equalToSuperview().offset(20)
+    }
+
+    ietTextView.snp.makeConstraints { make in
+      make.top.equalToSuperview().offset(10)
+      make.leading.equalTo(ietImageView.snp.trailing).offset(5)
+      make.trailing.bottom.equalToSuperview().offset(20)
+    }
+
+    return view
+  }()
+}
+
+var aboutARView: UIView = {
+  let view = UIView()
+  view.backgroundColor = Colors.lightGray
+  view.layer.cornerRadius = 25
+
+  let label = UILabel()
+  label.text = "Наведите камеру на изображение календаря"
+  label.textAlignment = .center
+  label.textColor = Colors.darkGray
+  label.backgroundColor = .clear
+  label.font = UIFont.systemFont(ofSize: 20, weight: .heavy)
+  label.numberOfLines = 2
+
+  view.addSubview(label)
+  label.snp.makeConstraints { $0.horizontalEdges.centerY.equalToSuperview() }
+
+  return view
+}()
