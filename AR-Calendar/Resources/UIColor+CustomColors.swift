38fc6dd2b128693e4b10c4e37d910a874b43a18a
Denis Beloshitskiy <beloshitskiy.denis@gmail.com>
2022-11-16 23:53:22 +0300

    Replace Assets to Resources

---
+++ AR-Calendar/Resources/UIColor+CustomColors.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+CustomColors.swift
+//  AR-Calendar
+//
+//  Created by Denis Beloshitskiy
+//
+
+import UIKit
+
+public enum Colors {
+  static let lightGray = UIColor(red: 233 / 255, green: 233 / 255, blue: 233 / 255, alpha: 1)
+  static let darkGray = UIColor(red: 97 / 255, green: 97 / 255, blue: 97 / 255, alpha: 1)
+}
